33064c2a9b6dc314f2c183f80d4c8f8aaf998c1e
syedmujtabanaqvi <nushat786syed@gmail.com>
2026-05-20 07:14:58 +0500

    add: Add like icon

--- src/screens/Search.jsx
+++ src/screens/Search.jsx
@@ -3,10 +3,10 @@ import { StyleSheet, Text, View, FlatList, Image } from 'react-native'
 import React from 'react'
 
 const posts = [
-    { id: '1', usernamename: 'fizzahthescorp', time:'8 minutes ago' , description:'Turkey trip was smooth, comfy stay and honestly a really good experience.',  profilepic: require('../../assets/image/turki.jpeg') ,image: require('../../assets/image/turki.jpeg') },
-      { id: '2',usernamename: 'mina khan', description:'Turkey trip was smooth, comfy stay and honestly a really good experience.', image: require('../../assets/image/ankarapic1.jpg') },
-   { id: '3', usernamename: 'Ali222', description:'Turkey trip was smooth, comfy stay and honestly a really good experience.',image: require('../../assets/image/ankarapic1.jpg') },
-      { id: '4', usernamename: 'kumail212',description:'Turkey trip was smooth, comfy stay and honestly a really good experience.', image: require('../../assets/image/ankarapic1.jpg') }
+    { id: '1', usernamename: 'fizzahthescorp', time:'8 minutes ago' , description:'Turkey trip was smooth, comfy stay and honestly a really good experience.',  profilepic: require('../../assets/image/fizzaimage.jpg') ,image: require('../../assets/image/turki.jpeg') },
+      { id: '2',usernamename: 'mina khan', description:'Turkey trip was smooth, comfy stay and honestly a really good experience.',profilepic: require('../../assets/image/fizzaimage.jpg') ,image: require('../../assets/image/ankarapic1.jpg')},
+   { id: '3', usernamename: 'Ali222', description:'Turkey trip was smooth, comfy stay and honestly a really good experience.',profilepic: require('../../assets/image/fizzaimage.jpg'),image: require('../../assets/image/ankarapic1.jpg') },
+      { id: '4', usernamename: 'kumail212',description:'Turkey trip was smooth, comfy stay and honestly a really good experience.',profilepic: require('../../assets/image/fizzaimage.jpg'), image: require('../../assets/image/ankarapic1.jpg')}
 ]
 
 
@@ -26,6 +26,7 @@ const Search = () => {
              <Image source={item.profilepic} style={styles.profilepicStyle} />
             <Image source={item.image} style={styles.imageStyle} />
             <Text style={styles.descriptionstyle}>{item.description}</Text>
+            <Image source={require('../../assets/image/icon-like.png') } style={styles.likeicon}/>
            
           </View>
         )}
@@ -57,6 +58,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginTop: 10,
     height: 300,
+    marginBottom:'10%',
     alignItems: 'center' 
   },
   imageStyle: {
@@ -65,28 +67,37 @@ const styles = StyleSheet.create({
     borderRadius: 8
   },
   profilepicStyle:{
+    height:45,
+    width:"12%",
+    borderRadius:50,
+    marginRight:'76%',
+    marginTop:"-11%",
+    marginBottom:'3%'
  
 
   },uName:{
 
 fontSize:18,
 color:'white',
-marginLeft:'-20%'
+marginRight:'25%'
 
   },
   Timestyles:{
-
 fontSize:13,
 color:'white',
-marginLeft:'-28%'
-
-
+marginRight:'35%'
   },
 
   descriptionstyle:{
 fontSize:15,
 color:'white',
 marginLeft:'5%',
-marginRight:'2%'
-  }
+marginRight:'2%',
+marginTop:'2%'
+  },likeicon:{
+    height:25,
+    width:'7%' ,
+     tintColor:'white' ,
+     marginRight:'76%',
+    marginTop:'4%'}
 })
